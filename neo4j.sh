940f072c67da7b1050f2aad299974dd04f31f644
Jilles van Gurp <jilles@jillesvangurp.com>
2017-02-17 16:32:06 +0100

    simple shellscript to run latest neo4j docker image

---
+++ neo4j.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p $HOME/neo4j/data $HOME/neo4j/logs
+docker run \
+    --publish=7474:7474 --publish=7687:7687 \
+    --volume=$HOME/neo4j/data:/data \
+    --volume=$HOME/neo4j/logs:/logs \
+    neo4j:latest
